ef7277e95538cb592cd95c1dc2515ba1758f7540
Gajendhiranm <48618592+Gajendhiranm@users.noreply.github.com>
2020-03-13 21:29:26 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// const cards =document.querySelector(".inner-container");
+// const  icons =document.querySelector(".icon-select");
+// cards.addEventListener("onmouseenter",function(){
+//    icons.style.visibilty="hidden";
+// })
+const button = document.querySelector("button");
+button.addEventListener("click",()=>{
+
+    
+})
